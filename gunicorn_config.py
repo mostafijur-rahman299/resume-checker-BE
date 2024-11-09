140808a3bf38f1a8d15c88c603dc565bb3e1b3ef
Mostafijur Rahman <mostafijur.rahman299@gmail.com>
2024-11-09 23:22:13 +0600

    New code for deployment

---
+++ gunicorn_config.py
@@ -0,0 +1,9 @@
+bind = "0.0.0.0:8000"
+module = "resume_checker.wsgi:application"
+
+workers = 4  # Adjust based on your server's resources
+worker_connections = 1000
+threads = 4
+
+certfile = "/etc/letsencrypt/live/resumechecker-be.mostafijur.xyz/fullchain.pem"
+keyfile = "/etc/letsencrypt/live/resumechecker-be.mostafijur.xyz/privkey.pem"
